7daf51badaa2acde671080012f41bbc14faecfa5
xixi197 Nova <xixi197@gmail.com>
2025-05-09 16:18:28 +0800

    feat: build ReactElement

---
+++ demos/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+	return <div>hello</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
